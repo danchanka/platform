6b8ecc539e2fd3a2cf5cf797dbb8b5d62a411e29
nomojenkins <jenkins@nomosoft.ru>
2021-04-05 16:16:04 +0300

    Deploy website - based on 3b72aaf3ec3120721d94db8164869ca4be8b1d97

---
+++ assets/js/081cbe88.8c6b6ba7.js
@@ -0,0 +1,1 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[30],{98:function(o){o.exports=JSON.parse('{"allTagsPath":"/platform/blog/tags","slug":"facebook","name":"facebook","count":1,"permalink":"/platform/blog/tags/facebook"}')}}]);
---
+++ assets/js/dcdd69e9.18565f7e.js
@@ -0,0 +1,1 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[593],{663:function(a){a.exports=JSON.parse('{"allTagsPath":"/platform/blog/tags","slug":"hola","name":"hola","count":1,"permalink":"/platform/blog/tags/hola"}')}}]);
