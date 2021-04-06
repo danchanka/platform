c3c976ddc63b943e01badec106c0ea2d9925755d
danchanka <danchenko@gmail.com>
2021-04-06 16:18:09 +0300

    Deploy website - based on 0de8bb1faf1dd88687f87fdc5ecc08abe7a60530

---
+++ assets/js/b6f409e0.bd891b6a.js
@@ -0,0 +1,1 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[488],{561:function(l){l.exports=JSON.parse('{"allTagsPath":"/platform/blog/tags","slug":"hello","name":"hello","count":2,"permalink":"/platform/blog/tags/hello"}')}}]);
