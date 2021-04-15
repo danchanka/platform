eca31295db57d00a2d198203ec461c55dca9cea8
nomojenkins <jenkins@nomosoft.ru>
2021-04-15 14:45:55 +0300

    Deploy website - based on 9f5b0fffa26ffe26e3d53382d9a250296e1f1b9a

--- ru/assets/js/c99b0e29.3969edbf.js
+++
@@ -1,1 +0,0 @@
-(window.webpackJsonp=window.webpackJsonp||[]).push([[536],{606:function(o){o.exports=JSON.parse('{"permalink":"/platform/ru/blog","page":1,"postsPerPage":10,"totalPages":1,"totalCount":3,"previousPage":null,"nextPage":null,"blogDescription":"Blog","blogTitle":"Blog"}')}}]);
